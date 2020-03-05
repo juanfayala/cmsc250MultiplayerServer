86320f419e9781ed4a19d2875dff413647efd206
Gabe G. <gabegullion@icloud.com>
2020-03-05 11:05:18 -0600

    Added (hopefully) useful comments to Simulation.java

--- src/main/java/edu/lawrence/pongserver/simulation/Simulation.java
+++ src/main/java/edu/lawrence/pongserver/simulation/Simulation.java
@@ -12,15 +12,20 @@ public class Simulation implements PongConstants {
     private Box boxTwo;
     private Lock lock;
     
+    // Constructs shapes
     public Simulation(int dX,int dY)
     {
-        outer = new Box(0,0,WIDTH,HEIGHT,false);
+        outer = new Box(0,0,WIDTH,HEIGHT,false); // Window border constraints
         ball = new Ball(WIDTH/2,WIDTH/2,dX,dY);
         boxOne = new Box(MARGIN,MARGIN, THICKNESS, LENGTH,true);
         boxTwo = new Box(WIDTH - MARGIN - THICKNESS,MARGIN, LENGTH, THICKNESS,true);
         lock = new ReentrantLock();
     }
     
+    /* If I understand correctly, this is where we would add our moving obstacles
+    *  Also where we check if the location of the ball is within a box?
+    *  We could also update the lives Label here
+    */
     public void evolve(double time)
     {
         lock.lock();
@@ -42,26 +47,37 @@ public class Simulation implements PongConstants {
         lock.unlock();
     }
     
+    // Player movement logic
     public void moveBox(int box,int deltaX,int deltaY)
     {
         lock.lock();
+        
+        // Determine which player is attempting to move
         Box mover = boxOne;
         if(box == 2)
             mover = boxTwo;
         
         double dX = deltaX;
         double dY = deltaY;
+        
+        // Move along x axis
         if(mover.x + deltaX < 0)
           dX = -mover.x;
+        // Keep the box within the window x axis border
         if(mover.x + mover.width + deltaX > outer.width)
           dX = outer.width - mover.width - mover.x;
        
+        // Move along y axis
         if(mover.y + deltaY < 0)
            dY = -mover.y;
+        // Keep the box within the window y axis border
         if(mover.y + mover.height + deltaY > outer.height)
            dY = outer.height - mover.height - mover.y;
         
+        // Move the box
         mover.move(dX,dY);
+        
+        // Not sure if we will need this
         if(mover.contains(ball.getRay().origin)) {
             // If we have discovered that the box has just jumped on top of
             // the ball, we nudge them apart until the box no longer
@@ -78,7 +94,7 @@ public class Simulation implements PongConstants {
         }
         lock.unlock();
     }
-    
+    // Useful test class
     public String getGameState() {
         Point ballLoc = ball.getRay().origin;
         
